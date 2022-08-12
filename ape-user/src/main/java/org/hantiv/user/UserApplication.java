44561748948c4f45e3c4e4466a0a4c77031c06ae
ZhikunHan <hanzk176@gmail.com>
2022-08-12 20:51:13 +0800

    Changes to be committed: 	modified:   ape-user/src/main/java/org/hantiv/user/UserApplication.java 	modified:   ape-user/src/main/java/org/hantiv/user/controller/TestController.java 	modified:   ape-user/src/main/resources/application.yml 	modified:   pom.xml
     Untracked files:
    	.idea/
    	ape-user/target/

--- ape-user/src/main/java/org/hantiv/user/UserApplication.java
+++ ape-user/src/main/java/org/hantiv/user/UserApplication.java
@@ -1,6 +1,16 @@
-package org.hantiv.user;/**
+package org.hantiv.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
  * @Author Zhikun Han
  * @Date Created in 21:08 2022/8/7
- * @Description: 
- */public class UserApplication {
+ * @Description:
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class);
+    }
 }
--- ape-user/src/main/java/org/hantiv/user/controller/TestController.java
+++ ape-user/src/main/java/org/hantiv/user/controller/TestController.java
@@ -1,6 +1,17 @@
-package org.hantiv.user.controller;/**
+package org.hantiv.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @Author Zhikun Han
  * @Date Created in 21:22 2022/8/7
- * @Description: 
- */public class TestController {
+ * @Description:
+ */
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello World";
+    }
 }
